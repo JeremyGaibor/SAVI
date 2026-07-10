e513f278d7691e878d2ad1db0437c0091b383dbe
Jeremy Gaibor <gaiborrodriguezjeremy@gmail.com>
2026-07-10 13:41:07 -0500

    fix: permitir acceso desde AWS

--- assistant/settings.py
+++ assistant/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-ev3^o*m=0*pa2x+1n^q^e!o7+k$!p)j(r=8ju=!2f7@or#jf1@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.1.104", "192.168.1.105"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "192.168.1.104",
+    "16.58.71.138",
+]
 
 
 # Application definition
